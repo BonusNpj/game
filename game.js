13c75a463dd5ba51cf28a30ed8c57ee1e988f473
BonusNpj <bonusforstudy01@gmail.com>
2025-08-02 18:45:00 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -9,60 +9,86 @@ var config = {
 
 var game = new Phaser.Game(config);
 var snake;
+var snakeBody = [];
+var snakeLength = 3;
 var food;
 var cursors;
 var score = 0;
 var scoreText;
+var dir = "RIGHT";
 
-function preload() { }
+function preload() {}
 
 function create() {
-    // สร้างงู
     snake = this.add.rectangle(300, 200, 10, 10, 0x00ff00);
     this.physics.add.existing(snake);
 
-    // สร้างอาหาร
     spawnFood(this);
-
-    // ควบคุม
     cursors = this.input.keyboard.createCursorKeys();
 
-    // คะแนน
     scoreText = this.add.text(10, 10, "คะแนน: 0", { fontSize: '20px', fill: '#fff' });
 
-    // ตั้งค่า update loop
     this.time.addEvent({
-        delay: 150, // ความเร็ว
+        delay: 150,
         loop: true,
         callback: () => moveSnake(this)
     });
 }
 
-function update() { }
+function update() {}
 
 function moveSnake(scene) {
-    if (cursors.left.isDown) {
-        snake.x -= 10;
-    } else if (cursors.right.isDown) {
-        snake.x += 10;
-    } else if (cursors.up.isDown) {
-        snake.y -= 10;
-    } else if (cursors.down.isDown) {
-        snake.y += 10;
-    }
+    // ควบคุม
+    if (cursors.left.isDown && dir !== "RIGHT") dir = "LEFT";
+    else if (cursors.right.isDown && dir !== "LEFT") dir = "RIGHT";
+    else if (cursors.up.isDown && dir !== "DOWN") dir = "UP";
+    else if (cursors.down.isDown && dir !== "UP") dir = "DOWN";
 
-    // ชนอาหาร
-    if (Phaser.Geom.Intersects.RectangleToRectangle(snake.getBounds(), food.getBounds())) {
+    // เพิ่มหัวใหม่
+    var newHead = scene.add.rectangle(snake.x, snake.y, 10, 10, 0x00ff00);
+
+    if (dir === "LEFT") newHead.x -= 10;
+    else if (dir === "RIGHT") newHead.x += 10;
+    else if (dir === "UP") newHead.y -= 10;
+    else if (dir === "DOWN") newHead.y += 10;
+
+    snakeBody.unshift(newHead);
+    snake = newHead;
+
+    // กินอาหาร
+    if (Phaser.Geom.Intersects.RectangleToRectangle(newHead.getBounds(), food.getBounds())) {
         score += 1;
         scoreText.setText("คะแนน: " + score);
         food.destroy();
         spawnFood(scene);
+        snakeLength++;
     }
 
-    // ออกนอกจอ → Game Over
-    if (snake.x < 0 || snake.x > 600 || snake.y < 0 || snake.y > 400) {
+    // ถ้าเกินความยาว → ลบหาง
+    if (snakeBody.length > snakeLength) {
+        var tail = snakeBody.pop();
+        tail.destroy();
+    }
+
+    // ชนตัวเอง
+    for (let i = 1; i < snakeBody.length; i++) {
+        if (Phaser.Geom.Intersects.RectangleToRectangle(newHead.getBounds(), snakeBody[i].getBounds())) {
+            scene.scene.restart();
+            snakeLength = 3;
+            score = 0;
+            dir = "RIGHT";
+            snakeBody = [];
+            return;
+        }
+    }
+
+    // ออกนอกจอ
+    if (newHead.x < 0 || newHead.x >= 600 || newHead.y < 0 || newHead.y >= 400) {
         scene.scene.restart();
+        snakeLength = 3;
         score = 0;
+        dir = "RIGHT";
+        snakeBody = [];
     }
 }
 
